6432d3bf6c888214be1cf6979c5c3460768f06fd
elis3o2 <eli.feuli37@gmail.com>
2026-06-17 19:13:21 -0300

    cambio vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/elis3o2/prode/', 
+  base: '/prode/', // <-- ASEGÚRATE DE QUE TENGA LAS DOS BARRAS '/'
 })
